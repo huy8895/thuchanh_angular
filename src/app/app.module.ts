568b9c95b8b42f26662edffc2945bf38ab3e4e8e
huy8895 <huy8895@gmail.com>
2020-10-23 09:09:37 +0700

    clone project from luan's repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InputSearchComponent } from './component/input-search/input-search.component';
+import { MessageComponent } from './component/message/message.component';
+import { UserAddComponent } from './users/user-add/user-add.component';
+import {FormsModule} from '@angular/forms';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    MenuComponent,
+    InputSearchComponent,
+    MessageComponent,
+    UserAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
