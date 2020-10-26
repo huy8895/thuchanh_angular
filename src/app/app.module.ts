8754599ad31580593280b3287d743d00492d8a2e
huy8895 <huy8895@gmail.com>
2020-10-26 08:58:36 +0700

    edit app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,9 @@ import {FormsModule} from '@angular/forms';
 import {HeroDetailComponent} from './components/hero-detail/hero-detail.component';
 import {MessagesComponent} from './components/messages/messages.component';
 import {AppRoutingModule} from './app-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
-import { HeroSearchComponent } from './components/hero-search/hero-search.component';
+import {HeroSearchComponent} from './components/hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -30,4 +29,5 @@ import { HeroSearchComponent } from './components/hero-search/hero-search.compon
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
