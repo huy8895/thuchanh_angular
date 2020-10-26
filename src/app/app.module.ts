264370a7e2ef43c8deba1e53cd0c7e0893c02283
huy8895 <huy8895@gmail.com>
2020-10-26 09:11:43 +0700

    view encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewEncapsulationComponent } from './view-encapsulation/view-encapsulation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewEncapsulationComponent
   ],
   imports: [
     BrowserModule
