55e783f4b8002017d92ecc0ba32661fd1f7017c7
huy8895 <huy8895@gmail.com>
2020-10-27 13:57:30 +0700

    cap nhat phan quyen va login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AppLayoutComponent} from './core/app-layout/app-layout.component';
+import {UserAddComponent} from './users/user-add/user-add.component';
+import {AdminGuard} from './admin/admin.guard';
 import {LoginComponent} from './core/login/login.component';
 import {UserEditComponent} from './users/user-edit/user-edit.component';
-import {UserAddComponent} from './users/user-add/user-add.component';
 import {UserListComponent} from './users/user-list/user-list.component';
 
 
-const routes: Routes = [
 
+const routes: Routes = [
+  {path: '', redirectTo: 'admin', pathMatch: 'full'},
   {
     path: 'admin',
     component: AppLayoutComponent,
@@ -23,6 +25,7 @@ const routes: Routes = [
         path: 'users/:id/edit', component: UserEditComponent
       },
     ],
+    canActivate: [AdminGuard]
   },
 
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { MenuComponent } from './core/menu/menu.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgxPaginationModule} from "ngx-pagination";
 import { InputSearchComponent } from './component/input-search/input-search.component';
 import { MessageComponent } from './component/message/message.component';
 import { UserAddComponent } from './users/user-add/user-add.component';
@@ -13,8 +15,6 @@ import { GroupListComponent } from './groups/group-list/group-list.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { AppLayoutComponent } from './core/app-layout/app-layout.component';
 import { LoginComponent } from './core/login/login.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgxPaginationModule} from 'ngx-pagination';
 
 @NgModule({
   declarations: [
