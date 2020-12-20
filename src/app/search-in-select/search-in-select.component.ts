178b184596c84a374b07ebfadec3af535e9484b1
HUYTV4 <huy8895@gmail.com>
2020-12-20 16:36:38 +0700

    update limit values to show in html

--- src/app/search-in-select/search-in-select.component.ts
+++ src/app/search-in-select/search-in-select.component.ts
@@ -12,15 +12,76 @@ export class SearchInSelectComponent implements OnInit {
     {id: 2, name: 'Kaunas'},
     {id: 3, name: 'Pavilnys', disabled: true},
     {id: 4, name: 'Pabradė'},
-    {id: 5, name: 'Klaipėda'}
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}, {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys', disabled: true},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'},
   ];
 
   selectedCity: any;
+  citiesOption: any;
 
   ngOnInit(): void {
   }
 
   submit(): void {
-    console.log(this.selectedCity);
+    let check: boolean;
+    this.cities.forEach(e => {
+      if (e.name === this.selectedCity) {
+        return check = true;
+      }
+    });
+    if (check){
+      console.log(this.selectedCity);
+    } else {
+      console.log(false);
+    }
+  }
+
+  // tslint:disable-next-line:typedef
+  search($event: Event) {
+    const key = $event.currentTarget;
+    console.log(key);
   }
 }
